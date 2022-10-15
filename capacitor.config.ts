95ebb7e221142f0a721a242d0aeceaa17f0a5e54
Carlos-Eduardo-S-O <75818652+Carlos-Eduardo-S-O@users.noreply.github.com>
2022-10-15 18:03:25 -0300

    initial commit, adding default list ionic template

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'yourlearn',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
